7ce2b6e88fb5d687e19b7cab8b623a9ed484c042
kitutune <p.kitutune@gmail.com>
2023-06-01 06:37:02 +0900

    追加コメント

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,15 +1,21 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+// NestJS および Prisma クライアントからのインポートを行います。
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common'; // NestJS コアモジュールから必要なインポートを行います
+import { PrismaClient } from '@prisma/client'; // Prisma クライアントをインポートします
 
-@Injectable()
+@Injectable() // Injectable デコレータは、このクラスが NestJS 依存性注入システムによって管理されるサービスであることを示します
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  // PrismaClient を拡張して、NestJS のライフサイクルイベントを利用する PrismaService を作成します
+
   async onModuleInit() {
-    await this.$connect();
+    // onModuleInit() メソッドは、NestJS モジュールが初期化されたときに自動的に実行されるライフサイクルメソッドです
+    await this.$connect(); // Prisma クライアントをデータベースに接続します
   }
 
   async enableShutdownHooks(app: INestApplication) {
+    // アプリケーションのシャットダウンフックを有効にするメソッド
     this.$on('beforeExit', async () => {
-      await app.close();
+      // 'beforeExit' イベントが発生する前に実行するリスナーを登録します
+      await app.close(); // NestJS アプリケーションを正常にシャットダウンします。これにより、すべてのモジュールの onDestroy() メソッドが呼び出されます
     });
   }
 }
--- src/task/models/task.model.ts
+++ src/task/models/task.model.ts
@@ -1,30 +1,28 @@
-import { Field, Int, ObjectType } from '@nestjs/graphql';
-import { Status } from '@prisma/client';
-// デコレータ
-// GraphQLスキーマ内でこのクラスをオブジェクト型として認識させるために必要
-@ObjectType()
+import { Field, Int, ObjectType } from '@nestjs/graphql'; // NestJSのGraphQLモジュールから必要なデコレータと型をインポートします
+import { Status } from '@prisma/client'; // PrismaクライアントからStatus enumをインポートします
+
+@ObjectType() // ObjectTypeデコレータは、このクラスがGraphQLのオブジェクト型であることを示します。これにより、GraphQLスキーマにこの型が自動的に含まれます
 export class Task {
-  // GraphQLの型
-  @Field(() => Int)
-  // typeの型
-  id: number;
+  // このクラスは、GraphQLスキーマで使用するタスクオブジェクトの形状を定義します
 
-  @Field()
-  name: string;
+  @Field(() => Int) // Fieldデコレータは、このプロパティがGraphQLフィールドであることを示します。ここでは、そのフィールドのGraphQL型をIntに設定しています
+  id: number; // このタスクの一意のID
 
-  @Field()
-  dueDate: string;
+  @Field() // このプロパティもGraphQLフィールドですが、型は自動的に推測されます（この場合、string）
+  name: string; // このタスクの名前
 
-  @Field()
-  // status: 'NOT_STARTED' | 'IN_PROGRESS' | 'COMPLETED';
-  status: Status;
-  // null許容
-  @Field({ nullable: true })
-  description: string;
+  @Field() // このプロパティもGraphQLフィールドです。型は自動的に推測されます（この場合、string）
+  dueDate: string; // このタスクの期日
 
-  @Field()
-  createdAt: Date;
+  @Field() // このプロパティもGraphQLフィールドです。型は自動的に推測されます（この場合、Status enum）
+  status: Status; // このタスクの現在のステータス
 
-  @Field()
-  updatedAt: Date;
+  @Field({ nullable: true }) // このプロパティもGraphQLフィールドですが、nullableオプションがtrueに設定されています。これは、このフィールドがnullであることを許可します
+  description: string; // このタスクの説明
+
+  @Field() // このプロパティもGraphQLフィールドです。型は自動的に推測されます（この場合、Date）
+  createdAt: Date; // このタスクが作成された日時
+
+  @Field() // このプロパティもGraphQLフィールドです。型は自動的に推測されます（この場合、Date）
+  updatedAt: Date; // このタスクが最後に更新された日時
 }
